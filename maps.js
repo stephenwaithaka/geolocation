e27beb12911dc26fe0d6b6d7833943731f0f914e
stephenwaithaka <stephen.waithaka@hotmail.com>
2015-12-15 17:39:00 +0200

    Added Clear Button

--- maps.js
+++ maps.js
@@ -1,8 +1,27 @@
-document.addEventListener("deviceReady", onDeviceReady, false);
+document.addEventListener("deviceready", onDeviceReady, false);
 
-var watchID = null;
+    var watchID = null;
 
-function onDeviceReady(){
-	var options = {timeout :30000};
-	watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
-}
+    // device APIs are available
+    //
+    function onDeviceReady() {
+        // Throw an error if no update is received every 30 seconds
+        var options = { timeout: 30000 };
+        watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
+    }
+
+    // onSuccess Geolocation
+    //
+    function onSuccess(position) {
+        var element = document.getElementById('geolocation');
+        element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
+                            'Longitude: ' + position.coords.longitude     + '<br />' +
+                            '<hr />'      + element.innerHTML;
+    }
+
+        // onError Callback receives a PositionError object
+        //
+        function onError(error) {
+            alert('code: '    + error.code    + '\n' +
+                  'message: ' + error.message + '\n');
+        }
